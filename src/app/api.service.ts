cb23813872cb32164d6906be6479942914300bce
sruthy-nr <sruthyn.@nestgroup.net>
2022-11-30 10:24:52 +0530

    addfriendapiintegration

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -11,5 +11,8 @@ export class ApiService {
     {
       return this.http.get("https://dummyapifriends.herokuapp.com/view")
     }
-  
+    addFriend =(dataToSend:any)=>
+    {
+      return this.http.post("https://dummyapifriends.herokuapp.com/adddata",dataToSend)
+    }
 }
